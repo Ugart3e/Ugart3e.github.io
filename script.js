10bfc8556dbfafc20c18a7d7f8e3ec94646446a7
Ugart3e <pepe.ugarte.galvez@gmail.com>
2023-11-10 18:41:48 +0100

    subida proyecta

---
+++ script.js
@@ -0,0 +1,36 @@
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else if (index >= slides.length) {
+        currentIndex = 0;
+    }
+
+    slides.forEach((slide, i) => {
+        if (i === currentIndex) {
+            slide.style.order = '1';
+        } else if (i === (currentIndex + 1) % slides.length || i === (currentIndex - 2 + slides.length) % slides.length) {
+            slide.style.order = '2';
+        } else {
+            slide.style.order = '3';
+        }
+    });
+}
+
+prevButton.addEventListener('click', () => {
+    currentIndex--;
+    showSlide(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex++;
+    showSlide(currentIndex);
+});
+
+showSlide(currentIndex);
